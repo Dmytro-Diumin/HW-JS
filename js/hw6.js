b989d5bd55da0efeefe5d0d90d27af03d7cdd970
Dmytro-Diumin <microb201196@gmail.com>
2023-12-24 20:49:08 +0200

    hw6

--- js/hw6.js
+++ js/hw6.js
@@ -582,17 +582,106 @@
 // console.log(user);
 
 // ===============================================
+// Задача 1
+// ==================
+
+const customer = {
+    username: "Mango",
+    balance: 24000,
+    discount: 0.1,
+    orders: ["Burger", "Pizza", "Salad"],
+    // Change code below this line
+    getBalance() {
+      return this.balance;
+    },
+    getDiscount() {
+      return this.discount;
+    },
+    setDiscount(value) {
+      this.discount = value;
+    },
+    getOrders() {
+      return this.orders;
+    },
+    addOrder(cost, order) {
+      this.balance -= cost - cost * this.discount;
+      this.orders.push(order);
+    },
+    // Change code above this line
+  };
+  
+  customer.setDiscount(0.15);
+  console.log(customer.getDiscount()); // 0.15
+  customer.addOrder(5000, "Steak");
+  console.log(customer.getBalance()); // 19750
+  console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// ===========================================================================================
+// Задача 2
+// ============================================
+
+class Storage {
+    #items;
+
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+    return this.#items;
+    }
+    addItem(newItem) {
+      this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+      this.#items.splice(1,1)
+    }
+
+};
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 
+// ===========================
+// Задача 3 
+// ===============================
 
+class StringBuilder {
+    #value;
+    constructor(value) {
+        this.#value = value;
+    }
 
+    getValue() {
+        return this.#value;
+    }
 
+    padStart(str) {
+        this.#value = `${str + this.#value}`;
+    }
 
+    padEnd(str) {
+        this.#value = `${this.#value + str}`;
+    }
 
+    padBoth(str) {
+        this.#value = `${str + this.#value + str}`;
+    }
+};
 
-
-
-
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
 
 
 
